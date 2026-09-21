054ce1fb21b47944466a43653fb482a0280a3e41
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-09-21 11:13:33 +0530

    javascript template literals

---
+++ javascript/day3 with basics/script.js
@@ -0,0 +1,50 @@
+let a = 20;
+let b = "20";
+
+console.log(typeof (a));
+console.log(`type of variable is ${a} and its type is  ${typeof (a)}`);
+console.log(`type of variable is ${b} and its type is  ${typeof (b)}`);
+
+var c = true;
+
+console.log("This is the type of the c  " + typeof (c))
+
+let p;
+
+console.log(typeof (p));
+
+let q = null;
+
+console.log(q);
+
+console.log(typeof (q))
+
+let city = "cspn";
+let names = "ameya";
+let job = "devops engineer";
+
+console.log("City of this person is " + city + " name of this person is " + names + " the job of this person is " + job);
+
+console.log(`City of this person is ${city} name of this person is ${names} and the job of this person is ${job}`)
+
+
+let pq= [20,12,56,21];
+
+console.log(typeof(pq));
+
+console.log(pq[2]+" "+pq[0]+" "+pq[3]);
+
+
+//object
+//it stores the data in key value pair format
+
+//water : it is a liquid important for life 
+
+let person={
+    name: "ameya",
+    sex: "male",
+    job: "devopsengineer",
+    mobilenumber: 92384789234
+};
+
+console.log(person.name +" "+person.sex+" "+person.job+" "+person.mobilenumber+" ")
