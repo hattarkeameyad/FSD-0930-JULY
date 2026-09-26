5135bb1dc21e30e9beed995e7d6dfdc83acc8ace
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-09-26 10:35:43 +0530

    while do while

---
+++ javascript/day 6 loops entry and exit contrlol/script.js
@@ -0,0 +1,21 @@
+/* while( condition of the loop){
+
+} */
+
+console.log("Before loop")
+let a = 10;
+while (a < 10) {
+    // console.log("loop started")
+    console.log("This is loop no :  ", a);
+    a++;
+
+
+}
+console.log("--------------------do while loop---------------------------");
+a = 10;
+do {
+    console.log("This is loop no :  ", a);
+    a++;
+
+} while (a < 10);
+// console.log("Loop finished")
